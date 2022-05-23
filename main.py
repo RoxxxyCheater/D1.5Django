78927ec1c468fa2b8a2be0fcdcc5e094fa058d23
RoxxxyCheater <sayt@3g.ua>
2022-05-24 02:53:53 +0300

    last update

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print("Hellow WOrlds")
-print("Buy!")dfgdf
+print("Buy!")
+
+# git config --global user.name "RoxxxyCheater"
+# git config --global user.email sayt@3g.ua
+
+#python manage.py migrate 
